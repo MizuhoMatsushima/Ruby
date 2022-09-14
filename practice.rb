60474e9b6c23e6c8a5707a0a5b528d5a44f14162
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-09-14 05:35:38 +0000

    [Add]practice

---
+++ practice.rb
@@ -0,0 +1,13 @@
+puts 'Hello, World!'
+
+for i in 1..15 do
+  if i % 3 == 0 and i % 5 == 0
+    puts 'FizzBuzz'
+  elsif i % 3 == 0
+    puts 'Fizz'
+  elsif i % 5 == 0
+    puts 'Buzz'
+  else
+    puts i
+  end
+end
