59dbbdc1324efc1d78a38c0a669306f509375da4
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-09-19 14:51:38 +0000

    [Add]2finish

---
+++ fizz_buzz.rb
@@ -0,0 +1,47 @@
+def fizz_buzz(num)
+  if num % 15 == 0
+    'FizzBuzz'
+  elsif num % 3 == 0
+    'Fizz'
+  elsif num % 5 == 0
+    'Buzz'
+  else
+    num.to_s
+  end
+end
+
+num = gets.to_i
+
+puts fizz_buzz(1)
+puts fizz_buzz(2)
+puts fizz_buzz(3)
+puts fizz_buzz(4)
+puts fizz_buzz(5)
+puts fizz_buzz(6)
+puts fizz_buzz(7)
+puts fizz_buzz(8)
+puts fizz_buzz(9)
+puts fizz_buzz(10)
+puts fizz_buzz(11)
+puts fizz_buzz(12)
+puts fizz_buzz(13)
+puts fizz_buzz(14)
+puts fizz_buzz(15)
+puts fizz_buzz(16)
+puts fizz_buzz(17)
+puts fizz_buzz(18)
+puts fizz_buzz(19)
+puts fizz_buzz(20)
+puts fizz_buzz(21)
+puts fizz_buzz(22)
+puts fizz_buzz(23)
+puts fizz_buzz(24)
+puts fizz_buzz(25)
+puts fizz_buzz(26)
+puts fizz_buzz(27)
+puts fizz_buzz(28)
+puts fizz_buzz(29)
+puts fizz_buzz(30)
+puts fizz_buzz(31)
+puts fizz_buzz(32)
+puts fizz_buzz(33)
