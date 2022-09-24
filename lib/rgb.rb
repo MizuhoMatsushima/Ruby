d0a4de0262e7530689cb48495fd1921540d19006
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-09-24 02:00:21 +0000

    [Add]4-11-1

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -14,3 +14,38 @@ def to_hex(r, g, b)
     n.to_s(16).rjust(2, '0')
   end
 end
+
+def to_ints(hex)
+  #r = hex[1..2]
+  #g = hex[3..4]
+  #b = hex[5..6]
+  #ints = []
+  #[r, g, b].each do |s|
+  #  ints << s.hex
+  #end
+  #ints
+
+  ###リファクタリング
+  #r = hex[1..2]
+  #g = hex[3..4]
+  #b = hex[5..6]
+  #[r, g, b].map do |s|
+  #  s.hex
+  #end
+
+  ###リファクタリング
+  #r, g, b = hex[1..2], hex[3..4], hex[5..6] #多重代入
+  #[r, g, b].map do |s|
+  #  s.hex
+  #end
+
+  ###リファクタリング
+  #hex.scan(/\w\w/).map do |s|
+  #  s.hex
+  #end
+
+  ###リファクタリング
+  hex.scan(/\w\w/).map(&:hex)
+end
+
+#p to_ints('#043c78')
