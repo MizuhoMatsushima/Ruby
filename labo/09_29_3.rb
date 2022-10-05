94d80151fcce4657008e224f66a84617542a885b
MizuhoMatsushima <105145800+MizuhoMatsushima@users.noreply.github.com>
2022-10-05 15:00:28 +0000

    [Fix]3問目のコードを修正

--- labo/09_29_3.rb
+++ labo/09_29_3.rb
@@ -1,10 +1,16 @@
-abc = 'ABCDEFJKQTVWXYZ'
-aet = [:A, :E, :T]
+abc = [:A, :B, :C, :D, :E, :F, :J, :K, :Q, :T, :V, :W, :X, :Y, :Z]
 
-aet.each do |n|
-  n.each 
+abc_5 = abc.repeated_combination(5).to_a
+
+cba = []
+
+abc_5.each do |abc|
+  if abc.include?(:A) && abc.include?(:E) && abc.include?(:T)
+    unless abc[1] == :A || abc[1] == :T || abc[3] == :E
+      cba << abc
+    end
+  end
 end
 
-for 
-
-puts n
+pp cba
+p cba.size
